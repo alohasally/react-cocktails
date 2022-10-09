c950c5350375ab85e1de235b7f4fec7c99bb835d
sally <peacheeks@gmail.com>
2022-10-09 23:51:23 +0900

    2nd

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,9 +1,33 @@
 import React from "react";
 import Cocktail from "cocktail";
 import Loading from "loading";
+
 import { useGlobalContext } from '../context';
+
 
 export default function CocktailList() {
   const {cocktails, loading} = useGlobalContext();
-  return <h1>cocktailList component</h1>;
+  if(loading) {
+    return <Loading/>
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
+  }
+
+  return (
+    <section className='section'>
+      <h2 className='section-title'>
+        cocktails
+      </h2> 
+      <div className='cocktails-center'>
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
+  )
 }
